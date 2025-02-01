e055f7138765907201357f93092d5adf3b1c0195
Samiksha Tiwari <incredibleitsme404@gmail.com>
2025-02-01 15:25:45 +0545

    .

--- src/Utils/ClientApi.js
+++ src/Utils/ClientApi.js
@@ -1,6 +1,24 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
+
+const getToken = () => localStorage.getItem('token');
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 //User API calls
 export const registerUser = async (userData) => {
     return axios.post(`${API_URL}/users/register`, userData);
